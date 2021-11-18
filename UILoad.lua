390ba11fd6f10cb731c1a9b1ecd497bbace9b9f6
ZoiIntra <94178333+ZoiIntra@users.noreply.github.com>
2021-11-18 15:48:07 +0700

    Update UILoad.lua

--- UILoad.lua
+++ UILoad.lua
@@ -811,7 +811,7 @@ function Kavo.CreateLib(kavName, themeList)
                 TextBox.ClearTextOnFocus = false
                 TextBox.Font = Enum.Font.FredokaOne
                 TextBox.PlaceholderColor3 = Color3.fromRGB(themeList.SchemeColor.r * 255 - 19, themeList.SchemeColor.g * 255 - 26, themeList.SchemeColor.b * 255 - 35)
-                TextBox.PlaceholderText = "Type here!"
+                TextBox.PlaceholderText = "Put Value Here"
                 TextBox.Text = ""
                 TextBox.TextColor3 = themeList.SchemeColor
                 TextBox.TextSize = 12.000
